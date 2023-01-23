6212579b5cfb4e11f6817cec622f53a50ddbe624
Mazin Fouad <m.fouad@live.de>
2023-01-23 10:26:38 +0100

    added played cards stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,6 +9,7 @@ import { Game } from '../models/game';
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
   game: Game;
+  currentCard: string = '';
 
   ngOnInit() {
     this.newGame();
@@ -20,6 +21,14 @@ export class GameComponent implements OnInit {
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.pickCardAnimation = true;
+      this.currentCard = this.game.stack.pop();
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,9 +7,9 @@ export class Game {
   constructor() {
     for (let i = 1; i < 14; i++) {
       this.stack.push(`ace_${i}`);
-      this.stack.push(`hearts${i}`);
-      this.stack.push(`clubs${i}`);
-      this.stack.push(`diamonds${i}`);
+      this.stack.push(`hearts_${i}`);
+      this.stack.push(`clubs_${i}`);
+      this.stack.push(`diamonds_${i}`);
       shuffle(this.stack);
     }
   }
